499a17c72db94af1d8cec1309b36a18800019910
Jenya Linx <sportpit2020@gmail.com>
2026-03-25 12:53:18 +0200

    fix pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,5 +1,16 @@
-import ReactPaginate from "react-paginate";
+import type { ComponentType } from "react";
+import ReactPaginateModule from "react-paginate";
+import type { ReactPaginateProps } from "react-paginate";
+
 import css from "./Pagination.module.css";
+
+type ModuleWithDefault<T> = { default: T };
+
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<
+    ComponentType<ReactPaginateProps>
+  >
+).default;
 
 interface PaginationProps {
   totalPages: number;
@@ -21,10 +32,10 @@ export default function Pagination({
       marginPagesDisplayed={1}
       onPageChange={({ selected }) => onChange(selected + 1)}
       forcePage={page - 1}
-      containerClassName={css.pagination}
-      activeClassName={css.active}
       nextLabel="→"
       previousLabel="←"
+      containerClassName={css.pagination}
+      activeClassName={css.active}
     />
   );
 }
